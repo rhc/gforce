cd20186adf3be59765f03433bde2a29982d0c8ca
Christian Kyony <ckyony@changamuka.com>
2014-11-30 19:14:46 +0200

    Set 60 minutes as default duration of training session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119104912) do
+ActiveRecord::Schema.define(version: 20141130171042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20141119104912) do
   create_table "training_sessions", force: true do |t|
     t.datetime "starts_at"
     t.string   "location"
-    t.integer  "minutes"
+    t.integer  "minutes",    default: 60
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "comments"
